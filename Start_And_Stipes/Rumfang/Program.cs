b76e4ddba17a721e874f6cefdc7672d8139260d4
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-08-10 18:04:07 +0200

    Added Rumfang

---
+++ Start_And_Stipes/Rumfang/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Rumfang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Rumfang";
+
+            Console.WriteLine("Indtast en højde:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nIndtast en længde:");
+            double lenght = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nIndtast en bredde:");
+            double width = Convert.ToDouble(Console.ReadLine());
+            double rumfang = height * width * lenght;
+            
+            Console.WriteLine($"\nRumfanget er: {rumfang}");
+            Console.ReadKey();
+        }
+    }
+}
