a9d3c601f0098ecd13841cc5bbd972a57e331ca8
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-08-10 18:34:38 +0200

    Added Fødselsdag

---
+++ Start_And_Stipes/Fødselsdag/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Fødselsdag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Fødselsdag";
+            //User input
+            Console.Write("Hvilket år er du født?: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nHvilken månede er du født i?: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nPå hvilken dag blev du født?: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            //Calculations
+            DateTime birthday = new DateTime(year, month, day);
+            DateTime now = DateTime.Now;
+
+            TimeSpan difference = now - birthday;
+            int years = (int)Math.Floor(difference.TotalDays / 365.2425);
+            birthday = birthday.AddYears(years);
+            difference = now - birthday;
+
+            int days = (int)Math.Floor(difference.TotalDays);
+
+
+            //Output
+            Console.WriteLine($"\nDu er {years} år og {days} dage gammel");
+            Console.ReadKey();
+        }
+    }
+}
